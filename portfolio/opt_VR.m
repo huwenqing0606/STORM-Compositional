f098d34b9d1e308aab777ecc87e059061fb3c239
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-06-08 13:21:28 -0500

    Add files via upload

--- portfolio/opt_VR.m
+++ portfolio/opt_VR.m
@@ -1,8 +1,8 @@
 %%Optimization For STORM-Compositional over portfolio optimization problem
 
-%code between separating dashed lines are for STORM, and are newly developed by us
-%code else where are adapted from SARAH-Compositional and not changed (see http://github.com/angeoz/SCGD) to make a comparison
-%we try to keep the original SARAH-Compositional code as much intact as we can
+%Due to the missing factor of 2 that we found in the original SARAH-C code (see http://github.com/angeoz/SCGD),
+%we have completely re-developed the whole optimization code, and we encapsulated the derivative calculations in several newly developed functions
+%Code between separating dashed lines are for STORM-C, elsewhere for SARAH-C and other previous algorithms
 
 %author: Jiaojiao Yang (Anhui Normal University)
 
@@ -144,19 +144,11 @@ function [g, G, F] = GD(data, w, batch_size)
     indexes = randperm(n);
     indexes = indexes(1:batch_size);
 %% compute g
-    g_mat = repmat(w, batch_size, 1);
-    g_mat(:, d+1) = data(indexes, :) * w';
-    g = mean(g_mat);
+    g = compute_g(data, w, indexes);
 %% compute G
-    G = diag(ones(d, 1));
-    G(d+1, :) = mean(data(indexes, :));
+    G = compute_G(data, w, indexes);
 %% compute F
-    mid = 2 * mean(data(indexes, :) * g(1:d)')- 2*g(d+1);
-                                   %the factor 2 in front of g(d+1) was missed everywhere in SARAH-C code, error?
-    r = mean(data(indexes, :));
-    F_dev = mean(diag(2 * data(indexes, :) * g(1:d)') * data(indexes, :)) - (2*g(d + 1)+1) * r;
-                                   %the factor 2 in front of g(d+1) was missed everywhere in SARAH-C code, error?
-    F_dev(d+1) = -mid;
+    F_dev = compute_gradf(data, g, indexes);
 %% compute gradient
     F = F_dev * G;
 end
@@ -168,21 +160,13 @@ function [g, G, F, y] = SCGD(data, w, y, batch_size, beta)
     indexes = randperm(n);
     indexes = indexes(1:batch_size);
 %% compute g
-    g_mat = repmat(w, batch_size, 1);
-    g_mat(:, d+1) = data(indexes, :) * w';
-    g = mean(g_mat);
+    g = compute_g(data, w, indexes);
 %% compute auxillary
     y = (1-beta) * y + beta * g;
 %% compute G
-    G = diag(ones(d, 1));
-    G(d+1, :) = mean(data(indexes, :));
+    G = compute_G(data, w, indexes);
 %% compute F
-    mid = 2 * mean(data(indexes, :) * y(1:d)')- 2*y(d+1);
-                                    %the factor 2 in front of y(d+1) was missed everywhere in SARAH-C code, error?
-    r = mean(data(indexes, :));
-    F_dev = mean(diag(2 * data(indexes, :) * y(1:d)') * data(indexes, :)) - (2*y(d + 1)+1) * r;
-                                    %the factor 2 in front of y(d+1) was missed everywhere in SARAH-C code, error?
-    F_dev(d+1) = -mid;
+    F_dev = compute_gradf(data, y, indexes);
 %% compute gradient
     F = F_dev * G;
 end
@@ -196,22 +180,14 @@ function [g, G, F, y] = ASCPG(data, w, w_t, y, batch_size, beta)
 %% update auxillary    
     z = (1-1/beta) * w_t + (1/beta)*w;
 %% compute g
-    g_mat = repmat(z, batch_size, 1);
-    g_mat(:, d+1) = data(indexes, :) * z';
-    g = mean(g_mat);
-    
+    g = compute_g(data, z, indexes);
+        %this is the main difference between ASC-PG and SCGD, w is replaced by z
 %% update auxillary
     y = (1-beta) * y + beta * g;
 %% compute G
-    G = diag(ones(d, 1));
-    G(d+1, :) = mean(data(indexes, :));
+    G = compute_G(data, w, indexes);
 %% compute F
-    mid = 2 * mean(data(indexes, :) * y(1:d)')- 2*y(d+1);
-                                    %the factor 2 in front of y(d+1) was missed everywhere in SARAH-C code, error?
-    r = mean(data(indexes, :));
-    F_dev = mean(diag(2 * data(indexes, :) * y(1:d)') * data(indexes, :)) - (2*y(d + 1)+1) * r;
-                                    %the factor 2 in front of y(d+1) was missed everywhere in SARAH-C code, error?
-    F_dev(d+1) = -mid;
+    F_dev = compute_gradf(data, y, indexes);
 %% compute gradient
     F = F_dev * G;
 end
@@ -223,32 +199,18 @@ function [g, G, F] = SARAH(data, w, w_t, g, G, F, batch_size)
     indexes = randperm(n);
     indexes = indexes(1:batch_size);
 %% compute g
-    g_mat = repmat(w, batch_size, 1);
-    g_mat(:, d+1) = data(indexes, :) * w';
-    
-    g_mat_t = repmat(w_t, batch_size, 1);
-    g_mat_t(:, d+1) = data(indexes, :) * w_t';
+    g_new = compute_g(data, w, indexes);
+    g_old = compute_g(data, w_t, indexes);
     g_t = g;
-    g = mean(g_mat) - mean(g_mat_t) + g;
+    g = g_new - g_old + g;
 %% compute G
     G_t = G;
     %Stay unchanged
 %% compute F
-    indexes = randperm(n);
+    indexes = randperm(n); %seems batchsize is chosen always to be 1 for grad f estimator? inherited from original SARAH-C code
     indexes = indexes(1);
-    mid = 2 * mean(data(indexes, :) * g(1:d)')- 2*g(d+1);
-                                    %the factor 2 in front of g(d+1) was missed everywhere in SARAH-C code, error?
-    r = mean(data(indexes, :));
-    F_dev = mean(diag(2 * data(indexes, :) * g(1:d)') * data(indexes, :)) - (2*g(d + 1)+1) * r;
-                                    %the factor 2 in front of g(d+1) was missed everywhere in SARAH-C code, error?
-    F_dev(d+1) = -mid;
-    
-    mid_t = 2 * mean(data(indexes, :) * g_t(1:d)')- 2*g_t(d+1);
-                                    %the factor 2 in front of g_t(d+1) was missed everywhere in SARAH-C code, error?
-    r_t = mean(data(indexes, :));
-    F_dev_t = mean(diag(2 * data(indexes, :) * g_t(1:d)') * data(indexes, :)) - (2*g_t(d + 1)+1) * r_t;
-                                    %the factor 2 in front of g_t(d+1) was missed everywhere in SARAH-C code, error?
-    F_dev_t(d+1) = -mid_t;
+    F_dev = compute_gradf(data, g, indexes);
+    F_dev_t = compute_gradf(data, g_t, indexes);
 %% compute gradient
     F = F_dev * G - F_dev_t * G_t + F;
 end
@@ -256,7 +218,6 @@ end
 %%% The only difference between SARAH and SVRG is that in SVRG, w_t, g_t,
 %%% G_t, F_t do not update in each iteration. So we don't have to write
 %%% another SVRG function
-
 
 
 %STORM-BEGIN-------------------------------------------------------------------------------------------------------------------------------
@@ -273,20 +234,11 @@ function [g, G, F] = STORM_GD(data, w, batch_size, ifreplace)
         indexes = indexes(1:batch_size);        %sample without replacement    
     end
 %% compute g
-    g_mat = repmat(w, batch_size, 1);
-    g_mat(:, d+1) = data(indexes, :) * w';
-    g = mean(g_mat);
+    g = compute_g(data, w, indexes);
 %% compute G
-    G = diag(ones(d, 1));
-    G(d+1, :) = mean(data(indexes, :));
+    G = compute_G(data, w, indexes);
 %% compute F
-    mid = 2 * mean(data(indexes, :) * g(1:d)')- 2*g(d+1);  
-            %the factor 2 in front of g(d+1) was missed everywhere in SARAH-C code, error?
-            %it seems to us that without this factor of 2 the behavior of STORM-C will not be good at data set data_cov_4 and data_cov_20
-    r = mean(data(indexes, :));
-    F_dev = mean(diag(2 * data(indexes, :) * g(1:d)') * data(indexes, :)) - (2*g(d + 1)+1) * r;
-            %the factor 2 in front of g(d+1) was missed everywhere in SARAH-C code, error?
-    F_dev(d+1) = -mid;
+    F_dev = compute_gradf(data, g, indexes);
 %% compute gradient
     F = F_dev * G;
 end
@@ -294,8 +246,8 @@ end
 
 %The STORM estimator for portfolio management problem
 function [g, G, F] = STORM(data, w, w_t, g, G, F, batch_size_g, batch_size_G, batch_size_F, a_g, a_G, a_F, ifreplace)
-    d = length(w);    %d is here N+1
-    n = size(data, 1);%n is here . The shape is n*d
+    d = length(w);
+    n = size(data, 1);%The shape is n*d
 %% compute g
     %Choose batch B_{t+1}^g of batch size batch_size
     if ifreplace == 1
@@ -305,16 +257,14 @@ function [g, G, F] = STORM(data, w, w_t, g, G, F, batch_size_g, batch_size_G, ba
        indexes = indexes(1:batch_size_g);        %sample without replacement    
     end
     %g(x_{t+1}, B_{t+1}^g) before taking the mean
-    g_mat = repmat(w, batch_size_g, 1);
-    g_mat(:, d+1) = data(indexes, :) * w';
+    g_new = compute_g(data, w, indexes);
     %g(x_t, B_{t+1}^g) before taking the mean
-    g_mat_t = repmat(w_t, batch_size_g, 1);
-    g_mat_t(:, d+1) = data(indexes, :) * w_t';
+    g_old = compute_g(data, w_t, indexes);
     %g_t
     g_t = g;
     %%calculate g_{t+1} = (1-a_g)g_t + a_g g(x_{t+1}, B_{t+1}^g) + (1-a_g)[g(x_{t+1}, B_{t+1}^g)-g(x_t, B_{t+1}^g)]
     %                   = (1-a_g)g_t + g(x_{t+1}, B_{t+1}^g) - (1-a_g)g(x_t, B_{t+1}^g)
-    g = (1-a_g)*g + mean(g_mat) - (1-a_g) * mean(g_mat_t);
+    g = (1-a_g)*g + g_new - (1-a_g) * g_old;
 %% compute G = partial g at steps t and t+1 
     %due to the particular structure of the portfolio optimization problem they are both constant matrices
     %stay unchanged
@@ -328,19 +278,50 @@ function [g, G, F] = STORM(data, w, w_t, g, G, F, batch_size_g, batch_size_G, ba
       indexes = indexes(1:batch_size_F);             %sample without replaceemnt
     end
     %calculate grad f at step t+1
-    mid = 2 * mean(data(indexes, :) * g(1:d)')- 2*g(d+1);
-    r = mean(data(indexes, :));
-    F_dev = mean(diag(2 * data(indexes, :) * g(1:d)') * data(indexes, :)) - (2*g(d + 1)+1) * r;
-    F_dev(d+1) = -mid;
+    F_dev = compute_gradf(data, g, indexes);
     %calculate grad f at step t
-    mid_t = 2 * mean(data(indexes, :) * g_t(1:d)')- 2*g_t(d+1);
-    r_t = mean(data(indexes, :));
-    F_dev_t = mean(diag(2 * data(indexes, :) * g_t(1:d)') * data(indexes, :)) - (2*g_t(d + 1)+1) * r_t;
-    F_dev_t(d+1) = -mid_t;
-    
+    F_dev_t = compute_gradf(data, g_t, indexes);
 %% compute F update, gradient
     %calculate F_{t+1}=(1-a_F)F_t + a_F G^Tgrad_f(x_{t+1}, B_{t+1}^f) + (1-a_F)[G^Tgrad_f(x_{t+1}, B_{t+1}^f)- G_t^Tgrad_f(x_t, B_t)]
     %                 =(1-a_F)F_t + G^Tgrad_f(x_{t+1}, B_{t+1}^f) - (1-a_F) G_t^Tgrad_f(x_t, B_t)               
     F = (1-a_F) * F + F_dev * G - (1-a_F) * (F_dev_t * G_t);
 end
+
+
+%compute the g function
+function [g] = compute_g(data, w, indexes)
+    d = length(w);
+    n = size(data, 1);%The shape is n*d    
+    batch_size = length(indexes);
+    g_mat = repmat(w, batch_size, 1);
+    g_mat(:, d+1) = data(indexes, :) * w';
+    g = mean(g_mat);
+end
+
+%compute the G function
+function [G] = compute_G(data, w, indexes)
+    d = length(w);
+    n = size(data, 1);%The shape is n*d    
+    batch_size = length(indexes);
+    G = diag(ones(d, 1));
+    G(d+1, :) = mean(data(indexes, :));
+end
+
+%compute the grad f function, denoted as F_dev
+function [F_dev] = compute_gradf(data, g, indexes)
+    d = length(g)-1;
+    mid = 2 * mean(data(indexes, :) * g(1:d)')- 2*g(d+1);
+                                   %the factor 2 in front of g(d+1) was missed everywhere in SARAH-C code, error?
+    r = mean(data(indexes, :));
+    F_dev = mean(diag(2 * data(indexes, :) * g(1:d)') * data(indexes, :)) - (2*g(d + 1)+1) * r;
+                                   %the factor 2 in front of g(d+1) was missed everywhere in SARAH-C code, error?
+                                   %it seems to us that without this factor of 2 the behavior of STORM-C will not be good at data set data_cov_4 and data_cov_20
+
+    F_dev(d+1) = -mid;
+end
 %STORM-END-------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
